e4bb841b038009a98f1194719f33fe619fd08a2c
sewookjung <tpdnrqkqh@naver.com>
2021-03-09 00:03:39 +0900

    [ADD] Logout firebase when click the logout button and redirect by useHistory

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,6 +1,17 @@
+import { authService } from "fbase";
 import React from "react";
+import { useHistory } from "react-router";
 
 const Profile = () => {
-  return <span>Profile</span>;
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log out</button>
+    </>
+  );
 };
 export default Profile;
