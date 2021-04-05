36642be0ffabe59a8c54558ea285f6f2d4f0e562
sewookJung <tpdnrqkqh@naver.com>
2021-04-05 20:41:46 +0900

    [ADD] Islogged setting value by firebase auth function and throw the props of logged status to Approuter function

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,19 @@
-import React from 'react';
-import AppRouter from './Router';
+import React, { useState } from 'react';
+import AppRouter from 'components/Router';
+import authService from 'fbase';
 
-const App = () => <AppRouter />;
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>
+        &copy; Niwtter
+        {new Date().getFullYear()}
+      </footer>
+    </>
+  );
+};
 
 export default App;
