545d5ec1fe5bd12e0587c71687773f19b9d8ccf4
sewookJung <tpdnrqkqh@naver.com>
2021-04-06 21:38:49 +0900

    [MODIFY] Change export default to export the authService object

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import AppRouter from 'components/Router';
-import authService from 'fbase';
+import { authService } from 'fbase';
 
 const App = () => {
   const [init, setInit] = useState(false);
