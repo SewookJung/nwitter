ac93591ed433ea0fa51b258533c0bde04bbd189e
sewookJung <tpdnrqkqh@naver.com>
2021-04-07 17:23:06 +0900

    Revert "[ADD] Import firebase database!"
    This reverts commit 6ae086023c7d2025bd1e50c322dba640c0dce215.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,29 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const Home = () => {
-  const [nweet, setNweet] = useState('');
+const Home = () => <span>Home</span>;
 
-  const onSubmit = (event) => {
-    event.preventDefault();
-  };
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setNweet(value);
-  };
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          placeholder="what is on your mind?"
-          maxLength={120}
-          onChange={onChange}
-        />
-        <input type="submit" value="Nweet" />
-      </form>
-    </div>
-  );
-};
 export default Home;
