ec4607baf4db4f93384a87ed995d588f7d69519c
sewookJung <tpdnrqkqh@naver.com>
2021-04-09 16:15:35 +0900

    [ADD] Display and update of user name

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,15 +1,44 @@
-import { authService } from 'fbase';
-import React from 'react';
+import { authService, dbService } from 'fbase';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const history = useHistory();
   const onLogoutClick = () => {
     authService.signOut();
     history.push('/');
   };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+  const onSubmit = async (event) => {
+    const {
+      target: { value },
+    } = event;
+
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
+  };
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Display Name!"
+          onChange={onChange}
+          value={newDisplayName}
+        />
+        <input type="submit" placeholder="Update Profile!" />
+      </form>
       <button type="button" onClick={onLogoutClick}>
         Logout
       </button>
