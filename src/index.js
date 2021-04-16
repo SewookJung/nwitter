ed824b67e4f2b63757c76335b571a385e6dc4810
sewookJung <tpdnrqkqh@naver.com>
2021-04-16 15:11:07 +0900

    [ADD] Import GlobalStyles component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/App';
+import GlobalStyles from 'components/styles/GlobalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GlobalStyles />
   </React.StrictMode>,
   document.getElementById('root')
 );
