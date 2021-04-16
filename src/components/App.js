656f8ca9868744f0d744fb452c804bf50a3be7a1
sewookJung <tpdnrqkqh@naver.com>
2021-04-16 15:12:21 +0900

    [ADD] Twitter icon and styled contents of foooter tag

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import AppRouter from 'components/Router';
 import { authService } from 'fbase';
+import { FaTwitterSquare } from 'react-icons/fa';
+import Colors from 'components/styles/Colors';
+import styled from 'styled-components';
+
+const Footer = styled.footer`
+  text-align: center;
+  padding: 20px 0px;
+  border-top: ${Colors.Border};
+  color: ${Colors.Twitter};
+`;
+
+const FooterTitle = styled.span`
+  font-weight: 700;
+  font-size: 20px;
+`;
+
+const FooterIcon = styled.div`
+  margin-top: 10px;
+  font-size: 20px;
+`;
 
 const App = () => {
   const [init, setInit] = useState(false);
@@ -14,6 +34,8 @@ const App = () => {
           uid: user.uid,
           updateProfile: (args) => user.updateProfile(args),
         });
+      } else {
+        setUserObj(null);
       }
       setInit(true);
     });
@@ -39,10 +61,12 @@ const App = () => {
       ) : (
         'Initializing...'
       )}
-      <footer>
-        &copy; Niwtter
-        {new Date().getFullYear()}
-      </footer>
+      <Footer>
+        <FooterTitle>Nwitter</FooterTitle>
+        <FooterIcon>
+          <FaTwitterSquare />
+        </FooterIcon>
+      </Footer>
     </>
   );
 };
